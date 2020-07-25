0a8a1140fb88d74e8500b22443e885c9331a47ef
Visibleon12 <visibleon12@gmail.com>
2020-07-25 22:30:09 +0530

    first commit

---
+++ src/build/precache-manifest.ca6a6f663bc91f78bd9e9aefd9b7d5f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09c4f2194f69eb42ebb6dc751385b7e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "981c7fb3c2ed5ef2596f",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "9117099dc2e63516961e",
+    "url": "/static/js/2.af9a9dba.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.af9a9dba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "981c7fb3c2ed5ef2596f",
+    "url": "/static/js/main.627788fc.chunk.js"
+  },
+  {
+    "revision": "bac2d675dfcb17431847",
+    "url": "/static/js/runtime-main.cb8ad503.js"
+  }
+]);
